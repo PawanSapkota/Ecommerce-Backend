0fd6d74fac2f3a22220540c39184d063f6ba86b9
Sakar Aryal <techiesakar@gmail.com>
2023-12-16 11:39:27 +0545

    fix: current date replaced with current ms

--- src/utils/Upload.ts
+++ src/utils/Upload.ts
@@ -1,27 +1,20 @@
 import * as multer from "multer";
-import {Request} from "express"
+import { Request } from "express";
 import AppError from "./AppError";
 
-const storage =multer.diskStorage({
-    destination:function(req:Request,file:Express.Multer.File,cb){
-        cb(null,`src/public`)
-    },
-    filename:function(req:Request,file:Express.Multer.File,cb){
-        console.log(file.originalname,"ggdhfd")
-        let newName=file.originalname.split(".")
-        console.log(newName[1],"array")
-        let date = new Date().toDateString()
-        console.log(date,"date")
-        let name= Date.parse(date)+'.'+newName[1]
-        console.log(name,"name")
-        cb(null,name );
-    }
-
-    
-
-})
+const storage = multer.diskStorage({
+  destination: function (req: Request, file: Express.Multer.File, cb) {
+    cb(null, `src/public`);
+  },
+  filename: function (req: Request, file: Express.Multer.File, cb) {
+    console.log(file.originalname, "ggdhfd");
+    let fileExtension = file.originalname.split(".")[1];
+    let milliSecond = Date.now();
+    let newName = milliSecond + "." + fileExtension;
+    cb(null, newName);
+  },
+});
 
 export const Upload = multer({
-    storage:storage,
-    
-})
+  storage: storage,
+});
